616612c93a46b4d7d9e86a47811cf98bf2684987
Ranga Sankar <47298258+rangasankar@users.noreply.github.com>
2024-01-05 21:34:14 -0500

    Update 3_🌐_chatbot_with_internet_access.py

--- pages/3_🌐_chatbot_with_internet_access.py
+++ pages/3_🌐_chatbot_with_internet_access.py
@@ -10,7 +10,7 @@ from langchain.callbacks import StreamlitCallbackHandler
 st.set_page_config(page_title="ChatWeb", page_icon="🌐")
 st.header('Chatbot with Internet Access')
 st.write('Equipped with internet access, enables users to ask questions about recent events')
-st.write('[![view source code ](https://img.shields.io/badge/view_source_code-gray?logo=github)](https://github.com/shashankdeshpande/langchain-chatbot/blob/master/pages/3_%F0%9F%8C%90_chatbot_with_internet_access.py)')
+st.write('[![view source code ](https://img.shields.io/badge/view_source_code-gray?logo=github)](https://github.com/rangasankar/len-AI/blob/master/pages/3_%F0%9F%8C%90_chatbot_with_internet_access.py)')
 
 class ChatbotTools:
 
