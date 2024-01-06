575a5a64d8d85994974a24ca3765cee401bdc6d6
Ranga Sankar <47298258+rangasankar@users.noreply.github.com>
2024-01-05 21:35:06 -0500

    Update 1_💬_basic_chatbot.py

--- pages/1_💬_basic_chatbot.py
+++ pages/1_💬_basic_chatbot.py
@@ -8,7 +8,7 @@ from langchain.chains import ConversationChain
 st.set_page_config(page_title="Storage Chatbot", page_icon="💬")
 st.header('Basic Storage Chatbot')
 st.write('Allows users to interact with the LLM/AI and enables you to get an understanding of Storage Devices')
-st.write('[![view source code ](https://img.shields.io/badge/view_source_code-gray?logo=github)](https://github.com/shashankdeshpande/langchain-chatbot/blob/master/pages/1_%F0%9F%92%AC_basic_chatbot.py)')
+st.write('[![view source code ](https://img.shields.io/badge/view_source_code-gray?logo=github)](https://github.com/rangasankar/len-AI/blob/master/pages/1_%F0%9F%92%AC_basic_chatbot.py)')
 
 class Basic:
 
