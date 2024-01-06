bed7d8f0c44f1cdcd219a99309694607321145ea
Ranga Sankar <47298258+rangasankar@users.noreply.github.com>
2024-01-05 21:29:35 -0500

    Update 2_⭐_context_aware_chatbot.py

--- pages/2_⭐_context_aware_chatbot.py
+++ pages/2_⭐_context_aware_chatbot.py
@@ -7,8 +7,8 @@ from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 
 st.set_page_config(page_title="Context aware chatbot", page_icon="⭐")
-st.header('Context aware chatbot')
-st.write('Enhancing Chatbot Interactions through Context Awareness')
+st.header('Context aware Storage chatbot')
+st.write('Enhancing  Storage Chatbot Interactions through Context Awareness')
 st.write('[![view source code ](https://img.shields.io/badge/view_source_code-gray?logo=github)](https://github.com/shashankdeshpande/langchain-chatbot/blob/master/pages/2_%E2%AD%90_context_aware_chatbot.py)')
 
 class ContextChatbot:
@@ -27,7 +27,7 @@ class ContextChatbot:
     @utils.enable_chat_history
     def main(self):
         chain = self.setup_chain()
-        user_query = st.chat_input(placeholder="Ask me anything!")
+        user_query = st.chat_input(placeholder="Ask me anything about Storage!")
         if user_query:
             utils.display_msg(user_query, 'user')
             with st.chat_message("assistant"):
